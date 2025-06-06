e3161cbbcb4823bf7205001dbbd3f492edd32126
moweilong <18550039021@163.com>
2025-06-06 14:24:19 +0800

    feat：引入Shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button>Click me</Button>
+
+<script>
+     import { Button } from "$lib/components/ui/button/index.js";
+</script>
